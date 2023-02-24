043cafe8e70ed62e1cc8eb98dafaf4fa3603e796
Pedro R <rochapedro436@gmail.com>
2023-02-23 22:00:37 -0300

    Alteracoes responsividade Menu

--- frontendbot/src/components/menu/menu.jsx
+++ frontendbot/src/components/menu/menu.jsx
@@ -1,24 +1,76 @@
 import { Link } from "react-router-dom"
 import "./menu.css"
-import robotIcon  from "../../assets/robotIcon.png"
-import {BiData} from "react-icons/bi"
-import {BsJournalText,BsCodeSlash} from "react-icons/bs"
-export default function Menu(){
-    return(
-        <>
-            <div className="leftMenu"> 
-            <section className="robotIcon">
-                <img src={robotIcon} ></img>
-            </section>
+import robotIcon from "../../assets/robotIcon.png"
+import { BiData } from "react-icons/bi"
+import { BsJournalText, BsCodeSlash } from "react-icons/bs"
+import { useState } from "react"
+import { FiAlignJustify } from 'react-icons/fi'
+
+
+
+export default function Menu() {
+    const [menuIconVisible, setMenuIconVisible] = useState(false)
+    const [hamburguerClickedClass, sethamburguerClickedClass] = useState("hamburguerUnclicked")
+
+    function renderizaLinksResponsivos(menuIconVisible) {
+        if (menuIconVisible) {
+
+            return <>
+
                 <section className="linksArea">
                     <BsJournalText size={38}></BsJournalText>
-                    <Link to="/">Estratégia</Link>
+                    <Link onClick={updateMenu} to="/">Estratégia</Link>
                 </section>
                 <section className="linksArea">
                     <BiData size={38}></BiData>
-                    <Link to="/data">Dados Salvos</Link>
+                    <Link onClick={updateMenu} to="/data">Dados Salvos</Link>
                 </section>
+            </>
+        }
+    }
+    const updateMenu = () => {
+        if (menuIconVisible) {
+            sethamburguerClickedClass("hamburguerUnclicked")
+        }
+        else {
+            sethamburguerClickedClass("hamburguerButtonClicked")
+        }
+        setMenuIconVisible(!menuIconVisible)
+
+
+    }
+    return (
+        <>
+            <div className="leftMenu">
+                <div className="desktopLinks">
+                    <section className="robotIcon">
+                        <img src={robotIcon} ></img>
+                    </section>
+                    <section className="linksArea">
+                        <BsJournalText size={38}></BsJournalText>
+                        <Link to="/">Estratégia</Link>
+                    </section>
+                    <section className="linksArea">
+                        <BiData size={38}></BiData>
+                        <Link to="/data">Dados Salvos</Link>
+                    </section>
+
+                </div>
+
             </div>
+            <div className="responsiveMenu">
+                <div className={hamburguerClickedClass}>
+                    <button onClick={updateMenu} >
+                        <FiAlignJustify size={45} ></FiAlignJustify>
+                    </button>
+
+                    {renderizaLinksResponsivos(menuIconVisible)}
+
+                </div>
+            </div>
+
+
+
         </>
     )
 }
